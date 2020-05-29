15fc56132f795c0b26f86cc24e16a8be3822a46f
joshua-vegas <vegas.joshua@gmail.com>
2020-05-29 10:27:02 +0200

    Deuxième version Domaine 1

--- java-adv-test/src/main/java/fr/natsystem/examen/services/data/PassengerCsvHandler.java
+++ java-adv-test/src/main/java/fr/natsystem/examen/services/data/PassengerCsvHandler.java
@@ -1,59 +1,24 @@
 package fr.natsystem.examen.services.data;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
 import fr.natsystem.examen.datamodel.Passenger;
 import fr.natsystem.examen.datamodel.PassengerClass;
 import fr.natsystem.examen.datamodel.PassengerSex;
-import fr.natsystem.examen.services.DataFileLoadingException;
 
 public class PassengerCsvHandler {
 	
-	// Récupération de la liste des passagers à partir d'un fichier csv 
-		public static List<Passenger> readPassengersFromCSVFile(File file) throws DataFileLoadingException {
-			List<String> lines = null;
-			try {
-				lines = Files.readAllLines(file.toPath());
-			}catch (IOException e) {
-				String detail = "aucun détail supplementaire";
-				if (e instanceof FileNotFoundException) {
-					detail = "le fichier n'est pas accessible";
-				}
-				if (e instanceof NoSuchFileException) {
-					detail = "le fichier n'a pas été trouvé";
-				}
-				throw new DataFileLoadingException("Le fichier contenant les passagers n'a pas pu être chargé correctement "
-						+ "(fichier en cause :" + file.getAbsolutePath() + ")"
-						+ "\ndétail de l'erreur :" + detail , e);
-			}
-			
-			// supprimme la ligne entête
-			lines.remove(0);
-			List<Passenger> passengers = new ArrayList<>();
-			for (String line : lines) {
-				Passenger passenger = PassengerCsvHandler.fromCSVLine(line);
-				passengers.add(passenger);
-			}
-			
-			// Appel de la méthode de tri
-			sortByPClass(passengers);
-		
-			return passengers;
-		}
-		
-
-		// Méthode permettant de créer des objets passenger à partir des informations présentes dans les lignes d'un fichier. 
-		public static Passenger fromCSVLine(String line) {
+	public static List<Passenger> readCSV(List<String> lines) {
+		List<Passenger> resultat = new ArrayList<>();
+		lines.remove(0);
+		for (String line : lines) {
 			String[] parts = line.split(";");
+			if (parts.length != 5) {
+				System.out.println("attention, le découpage de la ligne ne renvoie pas le résultat attendu");
+				System.out.println("ligne en question : " + line);
+				continue;
+			}
 			Passenger passenger = new Passenger();
-						
 			passenger.setName(parts[0].strip());
 			
 			// Condition de vérifications si le champs PClass du fichier n'est pas vide.
@@ -69,14 +34,19 @@ public class PassengerCsvHandler {
 			String isNumberBoolean = parts[4].strip();
 			Boolean isSurvived = "1".equals(isNumberBoolean);
 			passenger.setSurvived(isSurvived);
-			
-			return passenger;
+					
+			resultat.add(passenger);
 		}
-		
-		// Méthode de tri d'une liste de passagers basé sur la classe du passager.
-		public static List<Passenger> sortByPClass(List<Passenger> passengers) {
-			passengers.sort((i1,i2) -> i1.getPassengerClass().compareTo(i2.getPassengerClass()));
-			return passengers;
-		}
+		// Appel de la méthode de tri
+		sortByPClass(resultat);
+		return resultat;
+	}
+
+
+	// Méthode de tri d'une liste de passagers basé sur la classe du passager.
+	public static List<Passenger> sortByPClass(List<Passenger> passengers) {
+		passengers.sort((i1,i2) -> i1.getPassengerClass().compareTo(i2.getPassengerClass()));
+		return passengers;
+	}
 
 }
